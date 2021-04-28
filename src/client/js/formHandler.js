b9b79b0f69de906effed26db9e2a5e7914b61306
Emiri Begic <emiri.begic@gmail.com>
2021-04-29 00:39:53 +0200

    Update webpack config files

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,117 +1,124 @@
-const handleSubmit = async (event) => {
+const handleSubmit = async event => {
+	event.preventDefault();
+	// check what text was put into the form field
+	let cityName = document.getElementById('city').value;
 
-    event.preventDefault();
-    // check what text was put into the form field
-    let cityName = document.getElementById('city').value
+	// calculate how many days till trip departure
+	// ref: https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
+	// ref: https://www.w3resource.com/javascript/object-property-method/date.php
+	let startDate = document.getElementById('start').value;
+	let endDate = document.getElementById('end').value;
+	let today = new Date();
+	let tripStart = new Date(startDate);
+	let tripEnd = new Date(endDate);
+	let diffInTime = tripStart.getTime() - today.getTime();
+	// rounded up to the nearest integer (ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round)
+	let diffInDays = Math.round(diffInTime / (1000 * 3600 * 24));
+	// put dates in object to be accessible from everywhere
+	let dates = { startDate, endDate, diffInDays };
 
-        // calculate how many days till trip departure
-        // ref: https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
-        // ref: https://www.w3resource.com/javascript/object-property-method/date.php
-        let startDate = document.getElementById('start').value
-        let endDate = document.getElementById('end').value
-        let today = new Date()
-        let tripStart = new Date(startDate)
-        let tripEnd = new Date(endDate)
-        let diffInTime = tripStart.getTime() - today.getTime()
-        // rounded up to the nearest integer (ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round)
-        let diffInDays = Math.round(diffInTime / (1000*3600*24))
-        // put dates in object to be accessible from everywhere
-        let dates = {startDate, endDate, diffInDays}
+	// alert if a user submit without entering values
+	if (cityName === '') {
+		alert('Please enter a city name.');
+		return;
+	}
 
-        // alert if a user submit without entering values
-        if(cityName === "") {
-            alert('Please enter a city name.')
-            return
-        }
+	// check if a user enter a valid date
+	if (tripStart < today || tripEnd < tripStart) {
+		alert(
+			'Invalid date: either you select past date as start date or set end date earlier than start date.'
+		);
+		return;
+	}
 
-        // check if a user enter a valid date
-        if(tripStart < today || tripEnd < tripStart) {
-            alert('Invalid date: either you select past date as start date or set end date earlier than start date.')
-            return
-        }
+	console.log('::: Form Submitted :::');
 
-        console.log("::: Form Submitted :::")
+	// to make each fetched data be accessible from everywhere
+	let cityLatLon = {};
+	let receivedWeatherInJson = {};
+	let receivedPicInJson = {};
 
-        // to make each fetched data be accessible from everywhere
-        let cityLatLon = {}
-        let receivedWeatherInJson = {}
-        let receivedPicInJson = {}
+	console.log('Fetching geographical data from geonames:', {
+		city: cityName,
+	});
 
-        console.log('Fetching geographical data from geonames:', { city: cityName });
+	// これは{city: cityName}をjson形式でserverに送るアクション
+	// url,の後ろはoption, how to send post to the client, using POST methodとか
+	const resGeo = await fetch('/city', {
+		method: 'POST',
+		credentials: 'same-origin',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ city: cityName }),
+	});
+	try {
+		const receivedGeoInJson = await resGeo.json(); // receive res from the server side and transform into json
+		cityLatLon = {
+			city: receivedGeoInJson.geonames[0].name,
+			country: receivedGeoInJson.geonames[0].countryName,
+			lat: receivedGeoInJson.geonames[0].lat,
+			lon: receivedGeoInJson.geonames[0].lng,
+		};
+		console.log('Data received from geonames:', cityLatLon);
+		// return cityLatLon; //this will end implementation here
+	} catch (error) {
+		console.log('error', error);
+	}
 
-        // これは{city: cityName}をjson形式でserverに送るアクション
-        // url,の後ろはoption, how to send post to the client, using POST methodとか
-        const resGeo = await fetch('http://localhost:8081/city', {
-            method: 'POST',
-            credentials: 'same-origin',
-            mode: 'cors',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({city: cityName})
-        });
-        try {
-            const receivedGeoInJson = await resGeo.json(); // receive res from the server side and transform into json
-            cityLatLon = {
-                city: receivedGeoInJson.geonames[0].name,
-                country: receivedGeoInJson.geonames[0].countryName,
-                lat: receivedGeoInJson.geonames[0].lat,
-                lon: receivedGeoInJson.geonames[0].lng
-            }
-            console.log('Data received from geonames:', cityLatLon)
-            // return cityLatLon; //this will end implementation here
-        } catch (error) {
-            console.log('error', error);
-        }
+	console.log('Fetching weather data from weatherbit:', { cityLatLon });
 
-        console.log('Fetching weather data from weatherbit:', {cityLatLon});
+	// これはcityLatLonをjson形式でserverに送るアクション
+	const resWeather = await fetch('/weather', {
+		method: 'POST',
+		credentials: 'same-origin',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(cityLatLon),
+	});
+	try {
+		receivedWeatherInJson = await resWeather.json();
+		console.log('Data received from weatherbit:', receivedWeatherInJson);
 
-        // これはcityLatLonをjson形式でserverに送るアクション
-        const resWeather = await fetch('http://localhost:8081/weather', {
-            method: 'POST',
-            credentials: 'same-origin',
-            mode: 'cors',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(cityLatLon)
-        });
-        try {
-            receivedWeatherInJson = await resWeather.json();
-            console.log('Data received from weatherbit:', receivedWeatherInJson)
+		// return receivedWeatherInJson;
+	} catch (error) {
+		console.log('error', error);
+	}
 
-            // return receivedWeatherInJson;
-        } catch (error) {
-            console.log('error', error);
-        }
+	console.log('Fetching a pic from pixabay:', { cityLatLon });
 
-        console.log('Fetching a pic from pixabay:', {cityLatLon});
+	// これはcityLatLonをjson形式でserverに送るアクション
+	const resPic = await fetch('/pic', {
+		method: 'POST',
+		credentials: 'same-origin',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(cityLatLon),
+	});
+	try {
+		receivedPicInJson = await resPic.json();
+		console.log('Data received from pixabay:', receivedPicInJson);
 
-        // これはcityLatLonをjson形式でserverに送るアクション
-        const resPic = await fetch('http://localhost:8081/pic', {
-            method: 'POST',
-            credentials: 'same-origin',
-            mode: 'cors',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(cityLatLon)
-        });
-        try {
-            receivedPicInJson = await resPic.json();
-            console.log('Data received from pixabay:', receivedPicInJson)
+		// return receivedPicInJson;
+	} catch (error) {
+		console.log('error', error);
+	}
 
-            // return receivedPicInJson;
-        } catch (error) {
-            console.log('error', error);
-        }
+	const main = document.querySelector('main');
+	const tripData = document.createElement('section');
+	tripData.setAttribute('id', 'trip');
+	tripData.innerHTML = Client.updateUI(
+		dates,
+		receivedPicInJson,
+		cityLatLon,
+		receivedWeatherInJson
+	);
+	main.appendChild(tripData);
+};
 
-        const main = document.querySelector('main')
-        const tripData = document.createElement('section')
-        tripData.setAttribute('id', 'trip')
-        tripData.innerHTML = Client.updateUI(dates, receivedPicInJson, cityLatLon, receivedWeatherInJson);
-        main.appendChild(tripData)
-
-}
-
-export { handleSubmit }
+export { handleSubmit };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,70 +1,54 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin'); // configure webpack to copy all img resources to the dist folder
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
+	mode: 'production',
+	entry: './src/client/index.js',
+	optimization: {
+		minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
+	},
+	output: {
+		libraryTarget: 'var',
+		library: 'Client',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
 
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                  name: '[path][name].[ext]',
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            // refer below index.html
-            template: "./src/client/views/index.html",
-            // and create a new index.html in dist folder
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
-        new CopyPlugin({
-            patterns: [
-              { from: './src/client/media', to: './media' }
-            ]
-        }),
-        new MiniCssExtractPlugin({ filename: "[name].css" }),
-        new WorkboxPlugin.GenerateSW({
-            // Do not precache images
-            // exclude: [/\.(?:png|jpg|jpeg|svg)$/]
-            // exclude: [/\.map$/, /_redirects/]
-        })
-    ]
-}
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				loader: 'file-loader',
+				options: {
+					name: '[path][name].[ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			// refer below index.html
+			template: './src/client/views/index.html',
+			// and create a new index.html in dist folder
+			filename: './index.html',
+		}),
+		new CopyPlugin({
+			patterns: [{ from: './src/client/media', to: './media' }],
+		}),
+		new MiniCssExtractPlugin({ filename: '[name].css' }),
+		new WorkboxPlugin.GenerateSW({}),
+	],
+};

9e03873a981b2c946b6e391a768779a3f711d4c0
Emiri Begic <emiri.begic@gmail.com>
2021-06-16 07:34:25 +0200

    Delete old styles

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,3 @@ import './styles/main.scss';
 // js files
 import { init } from './js/controller';
 init();
-
-// import { handleSubmit } from './js/formHandler';
-// document.getElementById('submit').addEventListener('click', handleSubmit);
